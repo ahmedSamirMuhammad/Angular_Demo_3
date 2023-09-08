4675c6b854dc599a37a63b8607b9f6150ddcdcab
Ahmed <ahmed.samir00979@gmail.com>
2023-09-08 20:13:35 +0300

    removed commented code

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -30,15 +30,12 @@ export class CartComponent {
     const existingItem = this.cart.find((item) => item.id === product.id);
 
     if (existingItem) {
-      // If the product already exists in the cart, increment its quantity.
       existingItem.quantity++;
     } else {
-      // If the product is not in the cart, add it with a quantity of 1.
-      product.quantity = 1; // Initialize the quantity property
+      product.quantity = 1;
       this.cart.push({ ...product });
     }
 
-    // Update the counter and recalculate the total price
     this.calculateTotalPrice();
     this.incrementCounter();
   }
@@ -49,15 +46,12 @@ export class CartComponent {
     );
 
     if (existingItemIndex !== -1) {
-      // If the product exists in the cart and its quantity is greater than 1, decrement its quantity.
       if (this.cart[existingItemIndex].quantity > 1) {
         this.cart[existingItemIndex].quantity--;
       } else {
-        // If the quantity is 1 or less, remove the item from the cart.
         this.cart.splice(existingItemIndex, 1);
       }
 
-      // Decrement the counter when removing from the cart.
       this.productService.setCounterValue(--this.counter);
       this.calculateTotalPrice();
     }
@@ -69,16 +63,12 @@ export class CartComponent {
     );
 
     if (existingItemIndex !== -1) {
-      // Get the quantity of the item being removed
       const removedItemQuantity = this.cart[existingItemIndex].quantity;
 
-      // Remove the item from the cart
       this.cart.splice(existingItemIndex, 1);
 
-      // Decrease the counter by the quantity of items removed
       this.counter -= removedItemQuantity;
 
-      // Update the counter using the productService
       this.productService.setCounterValue(this.counter);
       this.calculateTotalPrice();
     }
